d939767d1c66375fa1d6c44a27a653e3c3ba4f1c
Saptagiri <stangudu@aptmail.com>
2017-11-06 15:56:55 -0500

    added partition count, compression details

--- view_Database_Table_Details.sql
+++ view_Database_Table_Details.sql
@@ -9,8 +9,14 @@ SELECT
 	,pf.name AS partitionFunctionName
 	,OBJECT_SCHEMA_NAME(i.object_id) AS schemaName	
 	,OBJECT_NAME(i.object_id) AS tableName
+	,t.create_date
 	,SUM(rows) as recordCount
-FROM    sys.indexes i
+	,count(*) as numOfPartitions
+	,convert(bit, max(case when p.data_compression = 3  then 1 else 0 end)) as pageCompressed
+	,convert(bit, max(case when p.data_compression = 4  then 1 else 0 end)) as columnStoreCompressed
+FROM    sys.tables t
+	INNER JOIN sys.indexes i
+		ON t.object_id = i.object_id
 	INNER JOIN sys.partitions p
 		ON i.object_id = p.object_id
 		AND i.index_id = p.index_id
@@ -25,5 +31,7 @@ AND  OBJECTPROPERTY(i.object_id,N'IsMSShipped')=0
 GROUP BY i.object_id
 	,object_name(i.object_id)
 	,d.name
-	,pf.name  
+	,pf.name
+	,t.create_date  
 ORDER BY SUM(rows) desc
+ 
